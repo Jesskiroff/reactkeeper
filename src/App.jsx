98e520cadc734d4430384ce583113ebf02aaab1d
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-21 16:53:03 -0500

    add css to note

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
+import Note from "./component/Note";
 function App(){
   return <div>
     <Header/>
+    <Note/>
     <Footer/>
   </div>
 }
