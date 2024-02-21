26403b314f2c4424412bad77252ca7714e3dd4c6
Jessica Kiroff <jesskiroff@gmail.com>
2024-02-21 16:22:34 -0500

    add dynamic copyright to footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "./component/Header";
-
+import Footer from "./component/Footer";
 function App(){
   return <div>
     <Header/>
+    <Footer/>
   </div>
 }
 
